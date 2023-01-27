79cb5776332a65060bc631b2bceeca977dd51cca
JosueAfouda <afouda.josue@gmail.com>
2023-01-27 18:52:41 +0100

    update app again

--- app.py
+++ app.py
@@ -5,13 +5,11 @@ from streamlit_pandas_profiling import st_profile_report
 
 from pycaret.regression import setup as setup_reg
 from pycaret.regression import compare_models as compare_models_reg
-from pycaret.regression import tune_model as tune_model_reg
 from pycaret.regression import save_model as save_model_reg
 from pycaret.regression import plot_model as plot_model_reg
 
 from pycaret.classification import setup as setup_class
 from pycaret.classification import compare_models as compare_models_class
-from pycaret.classification import tune_model as tune_model_class
 from pycaret.classification import save_model as save_model_class
 from pycaret.classification import plot_model as plot_model_class
 
