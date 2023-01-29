0a3f96fb603df7a489391b782f5b8d2ae3384765
JosueAfouda <afouda.josue@gmail.com>
2023-01-29 21:19:13 +0100

    remove import pandas_profiling

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import pandas_profiling
+#import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 
 from pycaret.regression import setup as setup_reg
